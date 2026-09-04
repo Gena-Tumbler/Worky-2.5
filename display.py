022cedb1b8a6a1c03c33415e419aa235bf4ac55b
Victor Schneider <victor.schneider.python@gmail.com>
2026-09-04 18:04:03 +0300

    Написаны фукции класса ButtonCommand кнопки старт и стоп а также логика работы приложения в кнопке старт. Написаны функции для работы приложения в файле function.py

--- display.py
+++ display.py
@@ -1,6 +1,12 @@
 from tkinter import *
 from PIL import Image, ImageTk
 import os
+import data_base as wdb
+import time
+import datetime
+
+
+
 
 class Window:
     """
@@ -20,6 +26,7 @@ class Window:
         Login = 'Гена Тумблер'
         counter_dst = '12345'
         counter_odo = '12345678'
+        status = 1
 
         Login_txt = Login
         counter_dst_txt = counter_dst
@@ -36,8 +43,69 @@ class Window:
         counter_dst_float_label.configure(fg='red')
         counter_odo_label = Label(fone_label, text=counter_odo_txt[0:6], font=('Roboto Bold', 13))
         counter_odo_label.place(x=156, y=102)
+        indication = Label(fone_label, text='STOP', fg='red', bg='black', font=('Roboto Bold', 20))
+        indication.place(x=3, y=130)
 
         self.window.mainloop()
+
+
+
+class ButtonCommand(Window):
+    """
+    Данный класс отвечает за выполнение команд взависимости от нажимаемых кнопок
+    """
+    @staticmethod
+    def command_button_start_status:
+        """
+        Изменения статуса выполнения после нажатия кнопки start
+        """
+        Window.status = 0
+
+    @staticmethod
+    def command_button_start:
+        """
+        Команда start отвечает за выполнение программы
+        """
+        while True:
+            Window.indication.configure(text='STOP', fg='red')
+            counter_data =wdb.Counter('Shock', 0, 0)
+            counter = counter_data.download(1)
+            counter_data.close()
+            Login, counter_dst, counter_odo = func.repac_download_counter(counter)
+            counter_odo = func.integer_to_string(counter_odo, 8)
+            counter_dst = func.integer_to_string(counter_dst, 5)
+            Window.counter_odo_label.configure(text=counter_odo[0:6])
+            Window.counter_dst_label.configure(text=counter_dst[0:3])
+            Window.counter_dst_float_label.configure(text=counter_dst[3])
+            int_odo = func.string_to_integer(counter_odo)
+            int_dst = func.string_to_integer(counter_dst)
+            Window.Login_label.configure(text=Login)
+            while Window.status == 0:
+                Window.indication.configure(text='DRIVE', fg='green')
+                Window.Login_lagel.configure(text=Login)
+                int_odo += 1
+                int_dst += 1
+                int_odo = func.big_number(int_odo, 100000000)
+                int_dst = func.big_number(int_dst, 100000)
+                str_odo = func.integer_to_string(int_odo, 8)
+                str_dst = func.integer_to_string(int_dst, 5)
+                Window.counter_odo_label.configure(text=str_odo[0:6])
+                Window.counter_dst_label.configure(text=str_dst[0:3])
+                Window.counter_dst_float_label.configure(text=str_dst[3])
+                counter_data_up = wdb.Counter(Window.Login, int_dst, int_odo)
+                counter_data_up.update()
+                counter_data_up.close()
+                time.sleep(1)
+
+    @staticmethod
+    def command_button_stop:
+        """
+        Команда stop отвечает за остановку выполнения программы
+        """
+
+
+        
+
 
 
 
---
+++ function.py
@@ -0,0 +1,42 @@
+"""
+Функции для работы приложения Воркометр
+"""
+
+def integer_to_string(counter, number):
+    """
+    Функция записи числа в формате текста с нужным количеством знаков
+    """
+    counter = str(counter)
+    testing_counter = len(counter)
+    if testing_counter < number:
+        need_number = number - testing_counter
+        new_counter = '0' * need_number + counter
+        return new_counter
+    elif testing_counter == number:
+        new_counter = counter
+        return new_counter
+
+def string_to_integer(counter):
+    """
+    Функция записи текстового числа в формате числовом
+    """
+    new_counter = int(counter)
+    return new_counter
+
+def repac_download_counter(pack_counter):
+    """
+    Функция распаковки загруженных данных
+    """
+    try:
+        Login, counter_odo, counter_dst = pack_counter[0][1], pack_counter[0][2], pack_counter[0][3]
+        return Login, counter_odo, counter_dst
+    except IndexError: #Если основная запись в бд повредиться
+        return 'Error', 0, 0
+
+def big_number(counter, n):
+    if counter == n:
+        counter = 0
+        return counter
+    else:
+        return counter
+
