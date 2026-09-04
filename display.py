28ece08d72d315a4344cc2d1c388c8fedd0c1446
Victor Schneider <victor.schneider.python@gmail.com>
2026-09-04 12:36:22 +0300

    Написаны временные файлы для пробного запуска приложения и написаны функции отображения данных приложения в файле display.py

--- display.py
+++ display.py
@@ -15,4 +15,29 @@ class Window:
         self.label.pack()
         self.window.geometry("380x188")
         self.window.title('Воркометр 2.5')
+
+        # Временные файлы
+        Login = 'Гена Тумблер'
+        counter_dst = '12345'
+        counter_odo = '12345678'
+
+        Login_txt = Login
+        counter_dst_txt = counter_dst
+        counter_odo_txt = counter_odo
+
+        fone_label = Label(self.window, image=self.img)
+        fone_label.place(x=0, y=0)
+        Login_label = Label(fone_label, text=Login_txt, font=('Roboto Bold', 12))
+        Login_label.place(x=5, y=150)
+        counter_dst_label = Label(fone_label, text=counter_dst_txt[0:3], font=('Roboto Bold', 12))
+        counter_dst_label.place(x=166, y=67)
+        counter_dst_float_label = Label(fone_label, text=counter_dst_txt[3], font=('Roboto Bold', 12))
+        counter_dst_float_label.place(x=194, y=67)
+        counter_dst_float_label.configure(fg='red')
+        counter_odo_label = Label(fone_label, text=counter_odo_txt[0:6], font=('Roboto Bold', 13))
+        counter_odo_label.place(x=156, y=102)
+
         self.window.mainloop()
+
+
+
